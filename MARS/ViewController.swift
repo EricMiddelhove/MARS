e2d176d1b7cd6ada102c2d21c4088be49d73ed42
ericmiddelhove <eric.middelhove@icloud.com>
2019-09-10 17:36:45 +0200

    Initial Commit

---
+++ MARS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MARS
+//
+//  Created by Eric Middelhove on 10.09.19.
+//  Copyright © 2019 Eric Middelhove. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
